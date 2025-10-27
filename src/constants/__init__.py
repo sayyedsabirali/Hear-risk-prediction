b48e16cb834f9de5e244e40390a99b0aa04a9770
SayyedSabir <sabirali969091@gmail.com>
2025-10-28 04:54:20 +0530

    24-ci-cd

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -28,8 +28,9 @@ TEST_FILE_NAME: str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 
-AWS_ACCESS_KEY_ID_ENV_KEY = os.getenv("AWS_KEY")
-AWS_SECRET_ACCESS_KEY_ENV_KEY = os.getenv("AWS_PASSWORD")
+AWS_ACCESS_KEY_ID_ENV_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY_ENV_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
 REGION_NAME = "us-east-1"
 
 
